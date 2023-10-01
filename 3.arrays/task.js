4425735e1d6d9744e61532f2114ee54732912417
Alex <kliver_sasha@mail.ru>
2023-10-01 18:37:09 +0300

    Внесены правки

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,8 @@
 function compareArrays(arr1, arr2) {
     if(arr1.length === arr2.length){
-        return arr1.every((el, i) => el === arr2[i])
-        }
-        else {
-        return false
+        return arr1.every((el, i) => el === arr2[i]);
+        } else {
+        return false;
     }
 }
 
@@ -11,5 +10,4 @@ function getUsersNamesInAgeRange(users, gender) {
     let result;
     let user;
   return result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => acc + item / arr.length, 0);
-;
 }
