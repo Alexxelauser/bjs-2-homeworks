c140eece656720c0de7ad1988e62ef3dd559cbc6
Alex <kliver_sasha@mail.ru>
2023-10-01 18:28:39 +0300

    Четвертое задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    if(arr1.length === arr2.length){
+        return arr1.every((el, i) => el === arr2[i])
+        }
+        else {
+        return false
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result;
+    let user;
+  return result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => acc + item / arr.length, 0);
+;
 }
